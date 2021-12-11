31a96b3c04e46b531b2b426d87842308d1b0f503
İsmail <ismailsilici@gmail.com>
2021-12-12 00:39:01 +0300

    tekrarlar
    tekrarlar

---
+++ tekrar/app.js
@@ -0,0 +1,154 @@
+//
+// let value;
+// let programmer = {
+//   name: "ismail",
+//   age:25,
+//   email:"ismayilgok16@gmail.com",
+//   langs:["python","java","html","css"],
+//   adress:{
+//     city:"ankara",
+//     street:"bahçe"
+//   },
+//   work:function(e){
+//     console.log("Programcı çalışıyor...");
+//   }
+// }
+//
+// console.log(programmer);
+// value = programmer.email;
+// console.log(value);
+// let value2 = programmer.work();
+//
+// const programmers = [
+//   {name:"ismail",age:25},
+//   {name:"oğuz",age:26}
+// ];
+//
+// value = programmers[0].name;
+// value = programmers[1].age;
+// console.log(value);
+
+
+let v;
+
+// const now = new Date();
+// console.log(now);
+
+const date1 = new Date("06-19-1993 06:15:00");
+console.log(date1);
+
+// const date2 = new Date("September 19 1993");
+// console.log(date2);
+
+// const date3 = new Date("1/19/1993")
+// console.log(date3);
+
+v = date1.getMonth();
+v= date1.getDate();
+v = date1.getDay()
+v = date1.getHours()
+v = date1.getMinutes()
+
+date1.setMonth(7)
+v = date1.getMonth()
+
+date1.setDate(17)
+v=date1.getDate()
+
+date1.setFullYear(1994);
+v = date1;
+
+date1.setHours(0);
+
+date1.setMinutes(17);
+
+date1.setSeconds(30);
+
+v = date1;
+
+
+
+
+
+console.log(v);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("");
